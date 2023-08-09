14dd162a58d0a149d001718199988df831d91cc1
Elbert <elbertnatan@hotmail.com>
2023-08-09 11:58:25 -0300

    - criando provider

--- flutter_favoriteplaces_app/lib/main.dart
+++ flutter_favoriteplaces_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_favoriteplaces_app/screens/places.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod/riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final colorScheme = ColorScheme.fromSeed(
@@ -28,7 +29,7 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-    const MyApp(),
+    const ProviderScope(child: MyApp()),
   );
 }
 
